4d969600a884aff5f0b326893f2fbcd88c17d0a4
Alex Ati <alejandro@atienzaramos.com>
2019-01-10 20:17:15 +0100

    Initial commit

---
+++ src/lein_with_deps_project/core.clj
@@ -0,0 +1,6 @@
+(ns lein-with-deps-project.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/lein_with_deps_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns lein-with-deps-project.core-test
+  (:require [clojure.test :refer :all]
+            [lein-with-deps-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
