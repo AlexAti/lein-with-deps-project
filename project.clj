c53ec02fc4c60b6ffcb00726fa60fda8f3e6f1ba
Alex Ati <alejandro@atienzaramos.com>
2019-01-10 20:21:19 +0100

    Added use of lein-tool-deps

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :plugins [[lein-tools-deps "0.4.3"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :repl-options {:init-ns lein-with-deps-project.core})
